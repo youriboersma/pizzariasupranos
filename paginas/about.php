add83dd677e42fb2ba0c42a3c63a2136a3a9ea26
Tim Smeets <tim.smeets.2004@gmail.com>
2024-11-27 12:25:06 +0100

    About Pagina
    volledig afgemaakt

--- paginas/about.php
+++ paginas/about.php
@@ -7,5 +7,94 @@ htmlnavbar();
 
 ?>
 
-</body>
+        <div class="about-right">
+        
+            <h1 style="text-align:center;">About</h1>
+
+            <div class="about-left">
+
+                <div class="about-over">
+                    <h3>Over Ons Bedrijf</h3>
+
+                    <br/>
+                    <p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    <br/>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+
+            <div class="about-right">
+                <div class="about-over">
+                    <h3>Bedrijfsinformatie</h3>
+
+                    <br/>
+                    <b>Bedrijfsnaam:</b> Pizzeria Soprano's<br/>
+                    <b>Adres:</b> Akerstraat 91 6456MA Kerkrade<br/>
+                    <b>Telefoonnummer:</b> 06 78422489<br/>
+                    <b>E-mailadres:</b> sopranos@pizza.nl<br/>
+                    <b>KVK-nummer:</b> 58776<br/>
+                    <b>BTW-id:</b> 477748<br/>
+                    <b>Klachtenregeling:</b> Heb je een klacht? Neem dan contact met ons op via sopranos@pizza.nl. Wij proberen binnen 3 werkdagen op uw klacht te reageren.<br/>
+                </div>
+                 
+            </div>
+
+        </div>
+        
+        <div class="about-left" align="left">
+
+            <div class="about-grid" align="left">
+                <!-- Derix -->
+                <div class="about-card">
+                    <img src="../images/about_derix.jpg" alt="Derix">
+                    <h3>Mario "Soprano" Derix</h3>
+                    <p>Eigenaar</p>
+                </div>
+
+                <!-- Corleone -->
+                <div class="about-card">
+                    <img src="../images/about_mark.jpg" alt="Corleone">
+                    <h3>Mark "Bosche bol" Corleone</h3>
+                    <p>Mede-eigenaar & accountant</p>
+                </div>
+
+                <!-- Bastiaens -->
+                <div class="about-card">
+                    <img src="../images/about_bastiaens.jpg" alt="Bastiaens">
+                    <h3>Bugsy Bastiaens</h3>
+                    <p>Kok</p>
+                </div>
+
+                <!-- Bollinger -->
+                <div class="about-card">
+                    <img src="../images/about_bollen.jpg" alt="Bollinger">
+                    <h3>John Bollinger</h3>
+                    <p>Kok</p>
+                </div>
+
+                <!-- Denissen -->
+                <div class="about-card">
+                    <img src="../images/about_denissen.jpg" alt="Denissen">
+                    <h3>Donny "Kray" Denissen</h3>
+                    <p>Kok & delivery</p>
+                </div>
+
+                <!-- Wagemans -->
+                <div class="about-card">
+                    <img src="../images/about_wagemans.jpg" alt="Wagemans">
+                    <h3>Kelly "Machine Gun" Wagemans</h3>
+                    <p>Delivery</p>
+                </div>
+
+                <!-- Nelson -->
+                <div class="about-card">
+                    <img src="../images/about_lennert.jpg" alt="Nelson">
+                    <h3>Lennert "Baby face" Nelson</h3>
+                    <p>Delivery</p>
+                </div>
+            </div>
+        </div>
+
+    </body>
 </html>
